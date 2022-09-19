60c5338e1376e638755c8a2dd7dbc92d07bc3f29
Justas V <valantinas.justas@gmail.com>
2022-09-19 09:13:36 +0300

    admin

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
 
+# registruoti, kad prieiti admin svetaineje
 # Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Genre)
+admin.site.register(Author)
+admin.site.register(BookInstance)
