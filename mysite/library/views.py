987b9d42f18f1dc64a8136592a49a9ecf13fa9b6
Justas V <valantinas.justas@gmail.com>
2022-09-20 16:45:04 +0300

    templates

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, BookInstance, Author, Genre
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Labas, pasauli! Pirmas Django app')
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+    num_instances_available = BookInstance.objects.filter(status='g').count()
+    # num_instances_available = BookInstance.objects.filter(status__exact='g').count()
+    num_authors = Author.objects.count()
+    kontext = {'num_books': num_books,
+               'num_instances': num_instances,
+               'num_instances_available': num_instances_available,
+               'num_authors': num_authors}
+
+    return render(request, 'index.html', context=kontext)
+
+
+def authors(request):
+    autoriai = Author.objects.all()
+    kontext = {
+        'authors': autoriai
+    }
+
+    return render(request, 'authors.html', context=kontext)
